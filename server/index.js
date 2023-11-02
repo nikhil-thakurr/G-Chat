6cfbcc81934b64b9e50e168778204da884eef68e
Nikhil Thakur <nikhilthakur@Nikhils-MacBook-Air.local>
2023-11-02 13:01:59 +0530

    Just Created Basic setup till now

---
+++ server/index.js
@@ -0,0 +1,23 @@
+const http=require("http");
+const express=require("express");
+const cors=require("cors");
+const  SocketIO  = require("socket.io");
+const app=express();
+
+const port=4500 || process.env.PORT 
+
+const server=http.createServer(app);
+
+app.get('/',(req,res)=>{
+    res.send("Working");
+})
+
+const io=SocketIO(server);
+
+io.on("connection",()=>{
+    console.log("New Connection");
+})
+
+server.listen(port,()=>{
+    console.log(`server is runnning http://localhost:${port}`);
+})
